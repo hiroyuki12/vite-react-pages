952d1502dd8faeac4a158abc658cd52e0693f92a
hiroyuki12 <hiroyuki12@gmail.com>
2022-07-16 10:33:06 +0900

    Test.

--- src/App.jsx
+++ src/App.jsx
@@ -21,7 +21,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Hello Edit <code>src/App.jsx</code> and save to test HMR
+          Edit <code>src/App.jsx</code> and save to test HMR
         </p>
       </div>
       <p className="read-the-docs">
