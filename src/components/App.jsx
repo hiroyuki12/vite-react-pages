73b8b839eca200b07ce4a8669e4bf3c2fdaa7fe1
hiroyuki12 <hiroyuki12@gmail.com>
2022-07-16 12:29:41 +0900

    Qiita API Show 2 Article.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 // axiosをインポート
 import axios from 'axios';
+import moment from 'moment';
 
 class App extends Component {
   constructor(props) {
@@ -26,9 +27,17 @@ class App extends Component {
       .then((response) => {
         // data にレスポンスから帰ってきた1つ目の記事の情報を格納
         const data = response.data[0];
+        const data1 = response.data[1];
         this.setState({
           title: data.title,
           url: data.url,
+          profile: data.user.profile_image_url,
+          created_at: data.created_at,
+
+          title1: data1.title,
+          url1: data1.url,
+          profile1: data1.user.profile_image_url,
+          created_at1: data1.created_at,
         });
         // コンソールから response と title と url を確認
         console.debug(response, "ressponse");
@@ -45,8 +54,14 @@ class App extends Component {
     return (
       <div className="App">
         <h1 className="app-title">Hello Qiita API</h1>
-        <p>タイトル: {this.state.title}</p>
-        <p>URL: <a target="__blank" href={this.state.url}>{this.state.url}</a></p>
+        <li className="item">
+          <img src={this.state.profile} width="50" height="50" loading="lazy" alt="img" />
+          <a className="QiitaApp-link" href={this.state.url} target="_blank" rel="noreferrer">{this.state.title}</a> {moment(this.state.created_at).fromNow()}
+        </li>
+        <li className="item">
+          <img src={this.state.profile1} width="50" height="50" loading="lazy" alt="img" />
+          <a className="QiitaApp-link" href={this.state.url} target="_blank" rel="noreferrer">{this.state.title1}</a> {moment(this.state.created_at1).fromNow()}
+        </li>
         <input
           type="button"
           value="検索"
