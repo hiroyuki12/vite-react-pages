18107c7b35c5514bb5ea03dfe407ca59a55b619f
hiroyuki12 <hiroyuki12@gmail.com>
2022-07-16 19:43:10 +0900

    Add Rate limit exceeded.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,6 +10,7 @@ function App() {
   const [postsList, setPostsList] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [tag, setTag] = useState('React');
+  const [error, setError] = useState('');
 
   // 一番下に到達したら handleClick()でページを更新
   const handleScroll = lodash.throttle(() => {
@@ -78,6 +79,8 @@ function App() {
         console.debug(url, "url")
       })
       .catch((error) => {
+        setIsLoading(false);
+        setError('Rate limit exceeded');
         console.debug(error);
       });
   }
@@ -109,6 +112,7 @@ function App() {
     return (
       <div className="App">
         <header className="QiitaApp-header">
+          <font color="red"><b>{error}</b></font><br />
           <a className="QiitaApp-link" href="https://mbp.hatenablog.com/entry/2022/07/16/103717" target="_blank" rel="noreferrer">netlifyでVite React App、QiitaAPIから記事情報を取得して表示</a><br />
           <a className="QiitaApp-link" href="https://mbp.hatenablog.com/entry/2022/07/14/225626" target="_blank" rel="noreferrer">Vite で React 新規プロジェクトを作成</a><br />
           <h3>QiitaでReactタグありの記事を表示</h3>
