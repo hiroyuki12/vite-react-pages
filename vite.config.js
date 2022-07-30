a20996428d708ff8189863071c4b22f45e254b9e
hiroyuki12 <hiroyuki12@gmail.com>
2022-07-30 10:14:56 +0900

    Add vite-plugin-pwa.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react()]
+  plugins: [react(), VitePWA()]
 })
