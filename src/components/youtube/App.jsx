dd5b08e7dd86140d101ce2eff7e6103c53c9d298
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-13 20:58:23 +0900

    Add YouTubeApp.

---
+++ src/components/youtube/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Api from './Api';
+
+function App() {
+  return (
+    <div className="App">
+      <a href="https://mbp.hatenablog.com/entry/2021/08/12/082102" target="_blank" rel="noreferrer">Youtube APIを使って検索＋再生</a><br />
+      <p>Youtube APIのテスト</p>
+      <Api/>
+    </div>
+  )
+}
+
+export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 //import App from './App'
-import App from './components/qiita/App'
+//import App from './components/qiita/App'
+import App from './components/youtube/App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
