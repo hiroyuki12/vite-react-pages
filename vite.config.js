3bc214e0e39321527f674004286b0af1ee713312
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-16 15:57:12 +0900

    Add png, manifest data.

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,53 @@ export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [react(), VitePWA()]
+  plugins: [react(), VitePWA({
+      includeAssets: [
+        'offline.html',
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
+      manifest: {
+        theme_color: '#000',
+        background_color: '#000',
+        display: 'standalone',
+        scope: '/',
+        start_url: '/',
+        short_name: 'Vite React App',
+        description:
+          'Vite React App',
+        name: 'Vite React App',
+        icons: [
+          {
+            src: '/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-256x256.png',
+            sizes: '256x256',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-384x384.png',
+            sizes: '384x384',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
 })
