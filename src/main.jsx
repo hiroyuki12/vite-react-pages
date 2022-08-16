cb392e293ce7751c16d237fe3de8cf2363a3bba9
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-16 15:46:19 +0900

    Add registerSW.

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,12 @@ import ReactDOM from 'react-dom/client'
 import App from './components/qiita/App'
 //import App from './components/youtube/App'
 import './index.css'
+import { registerSW } from 'virtual:pwa-register';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
+
+registerSW();
